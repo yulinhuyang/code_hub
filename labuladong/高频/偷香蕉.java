dacc601b14a66a25c609b137dd507b0ec237b018
yulinhuyang <yulinhuyang@163.com>
2021-01-22 21:19:57 +0800

    Create 偷香蕉.java

---
+++ labuladong/高频/偷香蕉.java
@@ -0,0 +1,29 @@
+int shipWithinDays(int[] weights, int D) {
+	// 载重可能的最小值
+    int left = getMax(weights);
+	// 载重可能的最大值 + 1
+    int right = getSum(weights) + 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (canFinish(weights, D, mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+// 如果载重为 cap，是否能在 D 天内运完货物？
+boolean canFinish(int[] w, int D, int cap) {
+    int i = 0;
+    for (int day = 0; day < D; day++) {
+        int maxCap = cap;
+        while ((maxCap -= w[i]) >= 0) {
+            i++;
+            if (i == w.length)
+                return true;
+        }
+    }
+    return false;
+}
