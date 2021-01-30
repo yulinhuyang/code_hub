bd6275e3e33b14396f45c100dcc018d300ea103e
yulinhuyang <yulinhuyang@163.com>
2021-01-30 20:24:25 +0800

    Create twoSum问题的核心思想.java

---
+++ labuladong/算法思维/twoSum问题的核心思想.java
@@ -0,0 +1,56 @@
+TwoSum I
+
+
+int[] twoSum(int[] nums, int target) {
+    int n = nums.length;
+    index<Integer, Integer> index = new HashMap<>();
+    // 构造一个哈希表：元素映射到相应的索引
+    for (int i = 0; i < n; i++)
+        index.put(nums[i], i);
+    
+    for (int i = 0; i < n; i++) {
+        int other = target - nums[i];
+        // 如果 other 存在且不是 nums[i] 本身
+        if (index.containsKey(other) && index.get(other) != i)
+            return new int[] {i, index.get(other)};
+    }
+    
+    return new int[] {-1, -1};
+}
+
+TwoSum II
+
+class TwoSum {
+    Set<Integer> sum = new HashSet<>();
+    List<Integer> nums = new ArrayList<>();
+
+    public void add(int number) {
+        // 记录所有可能组成的和
+        for (int n : nums)
+            sum.add(n + number);
+        nums.add(number);
+    }
+    
+    public boolean find(int value) {
+        return sum.contains(value);
+    }
+}
+
+
+总结 ，双指针
+
+int[] twoSum(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+        int sum = nums[left] + nums[right];
+        if (sum == target) {
+            return new int[]{left, right};
+        } else if (sum < target) {
+            left++; // 让 sum 大一点
+        } else if (sum > target) {
+            right--; // 让 sum 小一点
+        }
+    }
+    // 不存在这样两个数
+    return new int[]{-1, -1};
+}
