690aa60bae360a357daa398626f52487b3471570
yulinhuyang <yulinhuyang@163.com>
2021-01-30 19:22:43 +0800

    Update and rename 偷香蕉.java to 875_1011  偷香蕉 送包裹.java

---
+++ labuladong/高频/875_1011  偷香蕉 送包裹.java
@@ -0,0 +1,69 @@
+
+//875偷香蕉
+
+int minEatingSpeed(int[] piles, int H) {
+    // 套用搜索左侧边界的算法框架
+    int left = 1, right = getMax(piles) + 1;
+    while (left < right) {
+        // 防止溢出
+        int mid = left + (right - left) / 2;
+        if (canFinish(piles, mid, H)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+// 时间复杂度 O(N)
+boolean canFinish(int[] piles, int speed, int H) {
+    int time = 0;
+    for (int n : piles) {
+        time += timeOf(n, speed);
+    }
+    return time <= H;
+}
+
+int timeOf(int n, int speed) {
+    return (n / speed) + ((n % speed > 0) ? 1 : 0);
+}
+
+int getMax(int[] piles) {
+    int max = 0;
+    for (int n : piles)
+        max = Math.max(n, max);
+    return max;
+}
+
+
+//1011  送包裹
+int shipWithinDays(int[] weights, int D) {
+	// 载重可能的最小值
+    int left = getMax(weights);
+	// 载重可能的最大值 + 1
+    int right = getSum(weights) + 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (canFinish(weights, D, mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
+// 如果载重为 cap，是否能在 D 天内运完货物？
+boolean canFinish(int[] w, int D, int cap) {
+    int i = 0;
+    for (int day = 0; day < D; day++) {
+        int maxCap = cap;
+        while ((maxCap -= w[i]) >= 0) {
+            i++;
+            if (i == w.length)
+                return true;
+        }
+    }
+    return false;
+}
