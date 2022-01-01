23fedfc033c7dceac17359d152806fd5e288dda6
yulinhuyang <yulinhuyang@163.com>
2022-01-01 09:43:04 +0800

    Add files via upload

---
+++ CS_hub/CSAPP/code/ecf/forkprob7.c
@@ -0,0 +1,19 @@
+/* $begin forkprob7 */
+#include "csapp.h"
+int counter = 1;
+
+int main()
+{
+    if (fork() == 0)
+    {
+        counter--;
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+        printf("counter = %d\n", ++counter);
+    }
+    exit(0);
+}
+/* $end forkprob7 */
